10bbf2fdd72baf4867ff93137794219b6001cddf
EduardoArroyoGil <eduardoarroyogil@gmail.com>
2022-02-01 20:39:34 +0100

    Ejercicio 15 no terminado

--- your-code/main.py
+++ your-code/main.py
@@ -1,66 +1,78 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.__version__)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.randint(0,100, (2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+if a.size == b.size:
+    print("Yes a and b have the same size")
+else:
+    print("No a and b don't have the same size")
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#no we don't because they don't have the same shape
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = b.reshape(a.shape)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
 
+print(d)
 
+#c es un tensor de 2x3x5 y b no lo era, su shape era 5x2x3
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
+comparision = e == a
+if comparision.all():
+    print("E and A are equals")
+else:
+    print("E and A aren't equals")
 
-
-
+# They are equal because is multiplied by the 1 Tensor
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+
+f = np.empty(np.shape(d))
 
 
 
